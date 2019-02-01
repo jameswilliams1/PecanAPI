40934a43dc548c713503eb26c210d003c3636916
James Williams <zcapjwi@ucl.ac.uk>
2019-02-01 22:55:28 +0000

    updated readme and added sorting

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Pecan is toy web application for browsing customer data.
 
 ## Installation
 
-Pecan is built using Python 3. You will need to install the Flask framework:
+Pecan is built using Python 3. You will need to install the Flask framework and Flask-RESTful:
 
 ```
 pip3 install flask
+pip install flask-restful
 ```
 
 ## Starting the app
@@ -17,5 +18,9 @@ Run Pecan like this:
 ```
 FLASK_APP=main.py flask run
 ```
-
+Or run in debug mode using:
+```
+python main.py
+```
+from terminal.
 You can then access the web front end at [http://localhost:5000/]() and the API endpoint at [http://localhost:5000/api](). 
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import json, datetime
+import json
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 customers = json.load(open('customers.json', 'r'))
+
+
+def number_filter(operator, num1, num2):
+    try:
+        if operator.lower() == 'eq':
+            return num1 == num2
+        elif operator.lower() == 'gte':
+            return num1 >= num2
+        elif operator.lower() =='lte':
+            return num1 <= num2
+    except TypeError: # Catch None values
+        return False
 
 
 class Customer(Resource):
@@ -26,20 +38,38 @@ class Customers(Resource):
                 filtered_customers = list(filter(lambda c: str(c[key]).lower() == str(search_arguments[key]).lower(), filtered_customers))
             elif key == 'company' or key == 'first_name' or key == 'last_name' or key == 'profession': # Contains partial match (case-insensitive)
                 filtered_customers = list(filter(lambda c: str(search_arguments[key]).lower() in str(c[key]).lower() , filtered_customers))
-            elif key == 'age[gte]' or key == 'income[gte]' or key == 'wealth[gte]': # Customer value >= search value
-                key_sub = key.split('[', 1)[0]
-                print(key_sub)
-                print(search_arguments[key])
-                filtered_customers =  list(filter(lambda c: float(c[key_sub]) >= float(search_arguments[key]), filtered_customers))
+            elif 'age[' in key or 'income[' in key or 'wealth[' in key: # Filters numerical values using supplied operator
+                key_sub, operator = key.split('[', 1)
+                operator = operator[:-1]
+                try:
+                    compare_number = float(search_arguments[key])
+                except ValueError:
+                    return 'Bad request: {} is not a number.'.format(search_arguments[key]), 400
+                filtered_customers =  [c for c in filtered_customers if number_filter(operator, c[key_sub], compare_number)]
+            elif key == 'sort': # Sorts either direction with null values at the end
+                sort_value, direction = search_arguments['sort'].split('[', 1)
+                direction = direction[:-1]
+                is_desc = False
+                if direction == 'asc':
+                    pass
+                elif direction == 'desc':
+                    is_desc = True
+                else:
+                    return 'Bad request: {}. Sort direction must be asc or desc.'.format(search_arguments[key]), 400
+                no_value = list(filter(lambda c: c[sort_value] == None, filtered_customers))
+                filtered_customers = list(filter(lambda c: c[sort_value] != None, filtered_customers))
+                filtered_customers = sorted(filtered_customers, reverse=is_desc, key=lambda c: c[sort_value])
+                filtered_customers = filtered_customers + no_value
             else:
                 return 'Bad request: {} is not a recognised filter.'.format(key), 400
-
         return jsonify(filtered_customers)
 
 
-
-api.add_resource(Customer, "/customers/<int:id_number>") # Quickly find a customer by id number
-api.add_resource(Customers, "/customers/") # Search all customers
+api.add_resource(Customer, "/api/<int:id_number>") # Quickly find a customer by id number
+api.add_resource(Customers, "/api/") # Search all customers
+@app.route('/')
+def home():
+    return "<H1>Pecan API</H1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
